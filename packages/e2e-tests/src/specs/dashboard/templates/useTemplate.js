43518bbd1d48cd245178d27a844433de38826ebd
Brittany Feenstra <brittany.feenstra@formidable.com>
2022-03-22 08:49:40 -0700

    Revert "Floating Menus: Enable Experiment (#10989)"
    This reverts commit 7917e5aa8dac0322418ed9f803fdf9e4e01f0a2a.

--- packages/e2e-tests/src/specs/dashboard/templates/useTemplate.js
+++ packages/e2e-tests/src/specs/dashboard/templates/useTemplate.js
@@ -83,16 +83,13 @@ describe('Template', () => {
       text: /^Fresh/,
     });
 
-    // Open style pane
-    await expect(page).toClick('li[role="tab"]', { text: /^Style$/ });
-
     // Collapse layers popup to avoid aXe error about duplicative alt tags
     await expect(page).toClick('button', { text: /^Layers/ });
-    // close floating menu
-    await expect(page).toClick('button', { text: 'Dismiss menu' });
-
     // make sure popup is closed otherwise aXe will error
     await page.waitForTimeout(300);
+
+    // Open style pane
+    await expect(page).toClick('li[role="tab"]', { text: /^Style$/ });
 
     await expect(page).toMatchElement('input[placeholder="Add title"]');
     await expect(page).toMatchElement('[data-element-id]');
