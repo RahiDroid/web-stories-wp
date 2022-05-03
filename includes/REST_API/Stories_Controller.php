204eded9609b539e867f38d614cf9c2df864788c
Jonny Harris <jon@spacedmonkey.co.uk>
2022-05-03 17:04:32 +0100

    Prime attached objects

--- includes/REST_API/Stories_Controller.php
+++ includes/REST_API/Stories_Controller.php
@@ -288,7 +288,9 @@ class Stories_Controller extends Stories_Base_Controller {
 	public function get_items( $request ) {
 		add_filter( "rest_{$this->post_type}_query", [ $this, 'filter_query' ], 100, 2 );
 		add_filter( 'posts_clauses', [ $this, 'filter_posts_clauses' ], 10, 2 );
+		add_filter( 'posts_results', [ $this, 'prime_post_caches' ] );
 		$response = parent::get_items( $request );
+		remove_filter( 'posts_results', [ $this, 'prime_post_caches' ] );
 		remove_filter( 'posts_clauses', [ $this, 'filter_posts_clauses' ], 10 );
 		remove_filter( "rest_{$this->post_type}_query", [ $this, 'filter_query' ], 100 );
 
@@ -317,6 +319,39 @@ class Stories_Controller extends Stories_Base_Controller {
 		}
 
 		return $response;
+	}
+
+	/**
+	 * Prime post caches for attachments and parents.
+	 *
+	 * @since 1.21.0
+	 *
+	 * @param WP_Post[] $posts Array of post objects.
+	 * @return mixed Array of posts.
+	 */
+	public function prime_post_caches( $posts ) {
+		$post_ids = $this->get_attached_post_ids( $posts );
+		if ( ! empty( $post_ids ) ) {
+			_prime_post_caches( $post_ids );
+		}
+
+		return $posts;
+	}
+	/**
+	 * Get an array of attached post objects.
+	 *
+	 * @since 1.21.0
+	 *
+	 * @param WP_Post[] $posts Array of post objects.
+	 * @return int[] Array of post ids.
+	 */
+	protected function get_attached_post_ids( array $posts ): array {
+		$thumb_ids          = array_filter( array_map( 'get_post_thumbnail_id', $posts ) );
+		$publisher_logo_ids = array_filter( array_map( function ( $post ) {
+			return (int) get_post_meta( $post->ID, Story_Post_Type::PUBLISHER_LOGO_META_KEY, true );
+		}, $posts ) );
+
+		return array_unique( array_merge( $thumb_ids, $publisher_logo_ids ) );
 	}
 
 	/**
